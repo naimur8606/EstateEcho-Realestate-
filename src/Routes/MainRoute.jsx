ff0504bf6e1ab29b526e4d1d4dfeb0c690bc0aa9
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2023-11-24 16:35:00 +0600

    initial setup completed

---
+++ src/Routes/MainRoute.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>hello to gelo</div>,
+  }
+]);
+
+export default router;
