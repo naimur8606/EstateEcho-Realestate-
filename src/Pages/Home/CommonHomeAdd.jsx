0f24ae48c55e503ca0d709c53fab936552b35e81
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2023-11-24 22:35:09 +0600

    Common Title component created

--- src/Pages/Home/CommonHomeAdd.jsx
+++ src/Pages/Home/CommonHomeAdd.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import { GoLinkExternal } from 'react-icons/go'
+import CommonTitle from "../../Shared-Components/CommonTitle";
 
 const CommonHomeAdd = () => {
     return (
         <div className="my-10 w-[97%] lg:w-full mx-auto">
-             <h2 className="uppercase after:bg-slate-500 text-4xl font-medium text-center">Buy your beautiful apartment</h2>
-             <p className="h-0.5 w-1/2 bg-[#8bff11] mx-auto mt-2"></p>
+             <CommonTitle title={'Buy your beautiful apartment'}></CommonTitle>
              <p className="md:w-3/4 lg:w-2/3 mx-auto text-justify md:text-center my-8">Elevate your lifestyle with our luxurious apartments. Immerse yourself in contemporary design, unparalleled elegance, and breathtaking views. Don't miss out – buy your beautiful apartment today and embrace sophisticated living at its finest.</p>
              <div className="flex justify-center">
                 <Link to={"/properties"} className="flex items-center bg-[#8bff11] px-6 py-2 rounded-md font-semibold text-white">Explore All<GoLinkExternal className="ml-2"></GoLinkExternal></Link>
--- src/Shared-Components/CommonTitle.jsx
+++ src/Shared-Components/CommonTitle.jsx
@@ -1,8 +1,9 @@
 
-const CommonTitle = () => {
+const CommonTitle = ({title}) => {
     return (
         <div>
-            
+            <h2 className="uppercase after:bg-slate-500 text-4xl font-medium text-center">{title}</h2>
+             <p className="h-0.5 w-1/2 bg-[#8bff11] mx-auto mt-2"></p>
         </div>
     );
 };
